a85479a936a0e6b0a8fc2bd4b26b66f61ddd6434
tavbitok <63927409+tavbitok@users.noreply.github.com>
2020-04-19 23:20:00 +0300

    Update spec.js

--- spec.js
+++ spec.js
@@ -18,12 +18,12 @@ describe(' Automated tests using JS+Protractor, covering Registration form', fun
 	describe('Input invalid data in required fields Form of registration', function() {
 	
 		var home_page = require('home_page.js');
-		var inputData = ['user', '@', 'user@mail.c'];
-		var errorMessage = ['Username user is not available.', 'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.', 'Email is invalid or already taken'];
+		var inputData = ['user1', '@', 'user@mail.c'];
+		var errorMessage = ['Username user1 is not available.', 'Username may only contain alphanumeric characters or single hyphens, and cannot begin or end with a hyphen.', 'Email is invalid or already taken'];
 		
 		it('Input invalid data#1 in Username field', function() {
 			home_page.enterLogin(inputData[0]);
-			browser.sleep(2000);
+			//browser.sleep(2000);
 			var getErrorTextMessage = home_page.getErrorTextLogin;
 			expect(getErrorTextMessage).toBeTruthy(errorMessage[0]);
 			home_page.clearLogin();
@@ -31,7 +31,7 @@ describe(' Automated tests using JS+Protractor, covering Registration form', fun
 		
 		it('Input invalid data#2 in Username field', function() {
 			home_page.enterLogin(inputData[1]);
-			browser.sleep(2000);
+			//browser.sleep(2000);
 			var getErrorTextMessage = home_page.getErrorTextLogin;
 			expect(getErrorTextMessage).toBeTruthy(errorMessage[1]);
 			home_page.clearLogin();
@@ -39,7 +39,7 @@ describe(' Automated tests using JS+Protractor, covering Registration form', fun
 		
 		it('Input invalid data in Email field', function() {
 			home_page.enterEmail(inputData[2]);
-			browser.sleep(2000);
+			//browser.sleep(2000);
 			var getErrorTextMessage = home_page.getErrorTextEmail;
 			expect(getErrorTextMessage).toBeTruthy(errorMessage[2]);
 			home_page.clearEmail();
@@ -56,11 +56,11 @@ describe(' Automated tests using JS+Protractor, covering Registration form', fun
 		
 		it('Input valid data in Username field, Email field and click Registration button', function() {
 			home_page.enterLogin(validUser);
-			browser.sleep(2000);
+			//browser.sleep(2000);
 			home_page.enterEmail(validEmail);
-			browser.sleep(2000);
+			//browser.sleep(2000);
 			home_page.enterPassword(validPassword);
-			browser.sleep(2000);
+			//browser.sleep(2000);
 			home_page.clickRegistration();
 		});
 	});
